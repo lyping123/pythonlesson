9cc0d0a4785534ea1035f55d5148d985990e3fe8
leo lai <lyping0526@gmail.com>
2024-10-23 15:58:52 +0800

    python lesson11 search and delete

--- lesson11.py
+++ lesson11.py
@@ -13,23 +13,47 @@ mydb=mysql.connector.connect(
 mycursor=mydb.cursor()
 
 
-def select_data():
+def select_data(qry):
     try:
-        query="SELECT * FROM member_list"
-        mycursor.execute(query)
+        tree.delete(*tree.get_children())
+        mycursor.execute(qry)
         rows=mycursor.fetchall()
-        return rows
+        
+        for data in rows:
+            tree.insert("",tk.END,text=data[0],values=(data[1],data[2],data[3],data[4],data[5]))
     except mysql.connector.Error as error:
         messagebox.showerror("Error", f"Failed to select data from table: {error}")
 
 
+def search_data(search_text=None):
+    if search_text!=None:
+        query=f"SELECT * FROM member_list WHERE name LIKE '%{search_text}%' OR age LIKE '%{search_text}%' OR email LIKE '%{search_text}%' OR gender LIKE '%{search_text}%' OR address LIKE '%{search_text}%'"
+        return select_data(query)
+    else:
+        query="SELECT * FROM member_list"
+        return select_data(query)
+    
+def delete_data(event):
+    selected_item = tree.focus()
+    if selected_item:
+        confirmed=messagebox.askyesno("Confirmation","Are You sure you wan to delete this record?")
+        if confirmed:
+            text=tree.item(selected_item,"text")
+            primary_key_values=text
+            # print(primary_key_values)
+            qry="DELETE FROM member_list WHERE id=%s"
+            val=(primary_key_values,)
+            mycursor.execute(qry,val)
+            mydb.commit()
+            messagebox.showinfo("Success","Record deleted successfully")
+            search_data()
+        
+
 root=tk.Tk()
 root.geometry("400x400")
-datas=select_data()
 
 
 column_names = ['name','age','email','gender','address']
-
 
 
 tree=ttk.Treeview(root,columns=column_names)
@@ -38,6 +62,8 @@ search_label=tk.Label(text="Search").grid(row=0,column=0,sticky = "W",padx=10)
 search=tk.Entry(width=30)
 search.grid(row=1,column=0,sticky="w",pady=10,padx=10)
 
+search.bind("<KeyRelease>",lambda e:search_data(search.get()))
+
 tree.heading('#0', text='ID')
 tree.heading('name',text="name")
 tree.heading('age',text="age")
@@ -45,13 +71,9 @@ tree.heading('email',text="email")
 tree.heading('gender',text="gender")
 tree.heading('address',text="address")
 
-for data in datas:
-    tree.insert("",tk.END,text=data[0],values=(data[1],data[2],data[3],data[4],data[5]))
+tree.bind("<Double-1>",delete_data)
 
-
+search_data()
 tree.grid(row=3,column=0,columnspan=2,pady=10,padx=10)
+
 root.mainloop()
-
-
-        
-
